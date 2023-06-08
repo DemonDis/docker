84826c5b6e73f093ac175fb31e354aecf754b279
DemonDis <50297797+DemonDis@users.noreply.github.com>
2023-06-08 22:31:45 +0700

    ✨ cucmber + geckodriver

--- README.md
+++ README.md
@@ -11,6 +11,10 @@ docker build -t test-cucmber .
 ```
 ## 
 ```bash
+docker run test-cucmber python test_01.py
+```
+## 
+```bash
 docker run -it --rm --name test-cucmber test-cucmber
 ```
 ## 
--- test_01.py
+++ test_01.py
@@ -1,1 +1,27 @@
-print('Hi DOCKER!!!')
+from splinter import Browser
+from pytest_bdd import scenario, when
+import time
+
+browser = Browser('firefox')
+# browser = Browser('chrome')
+
+@scenario('test_01.feature', 'посещаю фронт')
+def test_посещаю_фронт():
+    """посещаю фронт."""
+    pass
+
+@when('я ya.ru', target_fixture="calc")
+def have_five():
+    """я ya.ru."""
+    browser.visit("http://www.ya.ru")
+    browser.find_by_css('input[class="search3__input mini-suggest__input"]').fill('TEXT EXAMPLE')
+    text_example = browser.find_by_css('span[class="a11y-hidden"]').text
+    time.sleep(3) #sleep for 3 sec
+    browser.screenshot()
+    browser.quit()
+    return text_example
+
+@when('я смотрю на картику')
+def add_three(calc):
+    print("CONSOLE LOG CALC =", calc)
+   
