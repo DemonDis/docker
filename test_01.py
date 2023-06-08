034ec9c4460515495181be9684ffaf4d4c717f9b
DemonDis <50297797+DemonDis@users.noreply.github.com>
2023-06-08 22:43:20 +0700

    ✨ geckodriver local

--- test_01.py
+++ test_01.py
@@ -1,9 +1,11 @@
-from splinter import Browser
+from selenium import webdriver
 from pytest_bdd import scenario, when
-import time
-
-browser = Browser('firefox')
-# browser = Browser('chrome')
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+#object of FirefoxOptions
+options = webdriver.FirefoxOptions()
+#set options.headless to True
+options.headless = True
+driver = webdriver.Firefox(executable_path="./drivers/geckodriver", options=options)
 
 @scenario('test_01.feature', 'посещаю фронт')
 def test_посещаю_фронт():
@@ -13,15 +15,10 @@ def test_посещаю_фронт():
 @when('я ya.ru', target_fixture="calc")
 def have_five():
     """я ya.ru."""
-    browser.visit("http://www.ya.ru")
-    browser.find_by_css('input[class="search3__input mini-suggest__input"]').fill('TEXT EXAMPLE')
-    text_example = browser.find_by_css('span[class="a11y-hidden"]').text
-    time.sleep(3) #sleep for 3 sec
-    browser.screenshot()
-    browser.quit()
+    driver.get("http://www.ya.ru")
+    text_example = '!!!!!!'
     return text_example
 
 @when('я смотрю на картику')
 def add_three(calc):
     print("CONSOLE LOG CALC =", calc)
-   
